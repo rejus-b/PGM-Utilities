537e7da553eb0460ea57b7285b3212d6a471e3be
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-05-23 15:58:49 +0100

    openWriteFile: BACKUP: Suddenly works with rectangles?? Literally changed no code

--- assignment_1/pgmTile.c
+++ assignment_1/pgmTile.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 	{
         /* exit the code */
         printf("ERROR: Miscellaneous (Bad tile layout)");
-		printf("\n %s \n", fileType);
+		// printf("\n %s \n", fileType);
         exit(EXIT_MISCELLANEOUS);		
 	}
 
