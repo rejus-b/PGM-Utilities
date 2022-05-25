1de41885535b2926a1aaaf019c3c671d15f6f475
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2022-05-25 15:12:25 +0100

    FLUSH

--- assignment_1/testscript.sh
+++ assignment_1/testscript.sh
@@ -8,55 +8,55 @@ make
 
 echo 
 echo "1: Testing pgmEcho for invalid inputs"
-./pgmEcho fakeTest fakeTest.pgm
+./pgmEcho images/fakeTest images/fakeTest.pgm
 echo -e "\n1 Result: Returned $?"
 echo
 
 echo "2: Testing pgmEcho for valid inputs"
-./pgmEcho slice0a.pgm realTest.pgm
+./pgmEcho images/slice0a.pgm images/realTest.pgm
 echo -e "\n2 Result: Returned $?"
-rm realTest.pgm
+rm images/realTest.pgm
 echo
 
 echo "3: Testing pgmComp for valid inputs"
-./pgmComp slice0a.pgm slice0a.pgm
+./pgmComp images/slice0a.pgm images/slice0a.pgm
 echo -e "\n3 Result: Returned $?"
 echo
 
 echo "4: Testing pgma2b"
-./pgma2b slice0a.pgm slice0b.pgm
-echo -e 
-./pgmComp slice0a.pgm slice0b.pgm
+./pgma2b images/slice0a.pgm images/slice0b.pgm
+echo
+./pgmComp images/slice0a.pgm images/slice0b.pgm
 echo -e "\n4 Result: Returned $?"
 echo
 
 echo "5: Testing pgmb2a"
-./pgmb2a slice0b.pgm newSlice0a.pgm
+./pgmb2a images/slice0b.pgm images/newSlice0a.pgm
 echo -e 
-./pgmComp slice0b.pgm newSlice0a.pgm
+./pgmComp images/slice0b.pgm images/newSlice0a.pgm
 echo -e "\n5 Result: Returned $?"
 echo
 
 echo "6: Testing pgmComp for non-equivalent pgms"
-./pgmComp slice0a.pgm notSlice0a.pgm
+./pgmComp images/slice0a.pgm images/notSlice0a.pgm
 echo -e "\n6 Result: Returned $?"
 echo
 
 echo "7: Testing pgmComp for non-equivalent pgms"
-./pgmComp slice0a.pgm slice0b.pgm
+./pgmComp images/slice0a.pgm images/slice0b.pgm
 echo -e "\n7 Result: Returned $?"
 echo
 
 echo "8: Testing pgmReduce for reduction factor 4"
-./pgmReduce baboon.ascii.pgm 4 reducedBaboon.ascii.pgm
+./pgmReduce images/baboon.ascii.pgm 4 images/reducedBaboon.ascii.pgm
 echo -e "\n8 Result: Returned $?"
-rm reducedBaboon.ascii.pgm
+rm images/reducedBaboon.ascii.pgm
 echo
 
 echo "9: Testing pgmTile for tile factor 4"
-./pgmTile baboon.ascii.pgm 4 "tiledBaboon.ascii.pgm_<row>_<column>.pgm"
+./pgmTile images/baboon.ascii.pgm 4 "images/tiledBaboon.ascii.pgm_<row>_<column>.pgm"
 echo -e "\n9 Result: Returned $?"
-rm tiledBaboon.*
+rm images/tiledBaboon.*
 echo
 
 echo "Running 'make clean'"
